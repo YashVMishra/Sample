5ec7ac6767e3dfbbac74bd80548f0793f9c4cb99
Yash Vardhan Mishra <yashvardhanmishra01@gmail.com>
2021-07-20 13:17:30 +0530

    testing ssh

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -2,7 +2,7 @@
 int main() {
     int arr[5]={3,4,1,2,5};
     for(int i=0;i<5-1;i++){
-        for(int j=0;j<5-1-i;j++){
+        for(int j=0;j<5-1-i;j++) {
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
